9d99e182653614fed3d5d0b35d7f9f94cb393a2b
Deepak Anand <thedanand@gmail.com>
2024-05-30 20:55:07 +0530

    add: fetchmain-get method endpoint

--- mr.py
+++ mr.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 import os
 import requests
 import time
-
+import boto3
+from openai import OpenAI
 
 load_dotenv()
 app = FastAPI()
@@ -24,12 +25,6 @@ app.add_middleware(
 
 # In-memory db
 users_db = {}
-
-
-
-
-
-
 
 @app.get('/')
 def home():
@@ -122,6 +117,16 @@ def view_assets(view_assets_input: ViewAssetsInput):
 
 
 
+@app.get("/fetchMainget")
+def fetch_main_get(email: str = Query(..., description="Email of the user")):
+    if email not in users_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"main_url": users_db[email].main}
+
+
+
+
+
 YOUR_API_KEY = os.environ.get("YOUR_API_KEY")
 
 class PromptInput(BaseModel):
@@ -134,27 +139,28 @@ def generate_3d_model(prompt_input: PromptInput):
     # First request to generate task ID
     headers = {
         "Authorization": f"Bearer {YOUR_API_KEY}",
-        "Content-Type": "application/json"
+        # "Content-Type": "application/json"
     }
     payload = {
-        "prompt": prompt,
         "mode": "preview",
+        "prompt": prompt,
         "art_style": "realistic",
         "negative_prompt": "low quality, low resolution, low poly, ugly"
     }
     response = requests.post(
-        "https://api.meshy.ai/v2/text-to-3d/generate", 
+        "https://api.meshy.ai/v2/text-to-3d", 
         headers=headers, 
         json=payload
     )
     response.raise_for_status()
-    task_id = response.json().get("task_id")
+    print(response.json())
+    task_id = response.json().get('result')
 
     if not task_id:
         raise HTTPException(status_code=500, detail="Failed to generate task ID")
 
    
-    time.sleep(20)
+    time.sleep(60)
 
     
     response = requests.get(
@@ -162,10 +168,19 @@ def generate_3d_model(prompt_input: PromptInput):
         headers=headers,
     )
     response.raise_for_status()
+    print(response.json())
     
     return response.json()
 
 
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app,port=int(os.environ.get('PORT', 8080)), host="0.0.0.0")
+    uvicorn.run(app,port=int(os.environ.get('PORT', 8080)), host="127.0.0.1")
